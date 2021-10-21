8c82949c3138c10208225606385f8090680b8da7
João Victor <joaovictorbellini33@gmail.com>
2021-10-20 22:18:58 -0300

    Update servidor.js

--- servidor/servidor.js
+++ servidor/servidor.js
@@ -6,4 +6,8 @@ app.get("/", function(req, res) {
     res.send("<h1>Servidor rodando com ExpressJS</h1>");
 });
 
+app.get("/cad", function(req, res) {
+    res.json( { mensagem: "Cadastro efetuado com sucesso!!!" } );
+});
+
 http.createServer(app).listen(3000, () => console.log("Servidor rodando local na porta 3000"));
